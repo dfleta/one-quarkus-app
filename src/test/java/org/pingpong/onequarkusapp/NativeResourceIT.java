667f2298ea0d5ac93bc1e466d2b229d02ad3ede7
dfleta <gelpiorama@gmail.com>
2021-06-14 02:27:40 +0200

    test(NativeResourceIT): Eliminar archivo ResourceTest por defecto

--- src/test/java/org/pingpong/onequarkusapp/NativeResourceIT.java
+++ src/test/java/org/pingpong/onequarkusapp/NativeResourceIT.java
@@ -3,7 +3,7 @@ package org.pingpong.onequarkusapp;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeResourceIT extends ResourceTest {
+public class NativeResourceIT extends ResourcesTest {
 
     // Execute the same tests but in native mode.
 }
